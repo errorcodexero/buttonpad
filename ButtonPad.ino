d816fd4686c3542c9c8563ec932b1be71e1eaa4b
Steve Tarr <stevet@RHUBARB>
2018-04-05 09:59:28 -0700

    added solve feature

--- ButtonPad.ino
+++ ButtonPad.ino
@@ -4,10 +4,13 @@
 // How many leds are in the strip?
 #define NUM_LEDS 64
 
-// Data pin that led data will be written out over      
+// Data pin that led data will be written out over
 #define DATA_PIN   5
 
 // pins for reading the buttons
+#define BUTTON_1   2
+#define BUTTON_2   3
+#define BUTTON_3   4
 #define RESET_PIN  8
 #define LD_PIN     9
 #define SS_PIN    10
@@ -22,17 +25,19 @@
 // This is an array of leds.  One item for each led in your strip.
 CRGB leds[NUM_LEDS];
 byte debounce[NUM_LEDS];
+byte pressed[NUM_LEDS];
 bool button[NUM_LEDS];
 byte gameboard[8][8];
 byte demoboard[8][8];
+byte increment;
 
 unsigned long when;
- 
+
 // define the order of the LEDs in the "strip"
 const byte ledSequence[] {
-   0,  1,  2,  3,  16, 17, 18, 19,
-   4,  5,  6,  7,  20, 21, 22, 23,
-   8,  9, 10, 11,  24, 25, 26, 27,
+  0,  1,  2,  3,  16, 17, 18, 19,
+  4,  5,  6,  7,  20, 21, 22, 23,
+  8,  9, 10, 11,  24, 25, 26, 27,
   12, 13, 14, 15,  28, 29, 30, 31,
   32, 33, 34, 35,  48, 49, 50, 51,
   36, 37, 38, 39,  52, 53, 54, 55,
@@ -43,21 +48,21 @@ const byte ledSequence[] {
 // define the order of the buttons
 // note that some of the pins were wired out-of-order
 const byte buttonSequence[] {
-   15, 14, 13, 12,   7,  6,  5,  4,
-   31, 30, 29, 28,  23, 22, 21, 20,
-   11, 10,  9,  8,   2,  3,  1,  0,
-   27, 26, 25, 24,  19, 18, 17, 16,
-   63, 62, 61, 60,  55, 54, 53, 52,
-   47, 46, 45, 44,  39, 38, 37, 36,
-   43, 42, 41, 40,  35, 34, 33, 32,
-   59, 58, 57, 56,  51, 50, 49, 48,
+  15, 14, 13, 12,   7,  6,  5,  4,
+  31, 30, 29, 28,  23, 22, 21, 20,
+  11, 10,  9,  8,   2,  3,  1,  0,
+  27, 26, 25, 24,  19, 18, 17, 16,
+  63, 62, 61, 60,  55, 54, 53, 52,
+  47, 46, 45, 44,  39, 38, 37, 36,
+  43, 42, 41, 40,  35, 34, 33, 32,
+  59, 58, 57, 56,  51, 50, 49, 48,
 };
 
 const CRGB indexedColor[] {
   CRGB::Black, CRGB::Red,     CRGB::Green,  CRGB::Blue,
   CRGB::Cyan,  CRGB::Magenta, CRGB::Yellow, CRGB::White,
 };
-  
+
 void showBoard(byte board[8][8])
 {
   for (byte i = 0; i < NUM_LEDS; i++) {
@@ -72,9 +77,9 @@ void rect( byte n, byte m, byte color )
 {
   if (n < 0 || n >= 8 || m < 0 || m >= 8) return;
   demoboard[n][m] = color;
-  demoboard[7-n][m] = color;
-  demoboard[n][7-m] = color;
-  demoboard[7-n][7-m] = color;
+  demoboard[7 - n][m] = color;
+  demoboard[n][7 - m] = color;
+  demoboard[7 - n][7 - m] = color;
 }
 
 void shrink()
@@ -153,7 +158,7 @@ void bigBang()
   rect(3, 3, 1);
   showBoard(demoboard);
   delay(40);
-  
+
   rect(2, 2, 1);
   rect(2, 3, 3);
   rect(3, 2, 2);
@@ -209,7 +214,7 @@ void bigBang()
 void attract()
 {
   memcpy(demoboard, gameboard, sizeof demoboard);
-  
+
   shrink();
   showBoard(demoboard);
   delay(100);
@@ -255,13 +260,44 @@ void attract()
   delay(40);
 }
 
+void solve()
+{
+  bool solved = false;
+  increment = 1;
+  do {
+    solved = true;
+    for (byte i = 0; i < NUM_LEDS; i++) {
+      if (pressed[i] != 0) {
+        solved = false;
+        byte row = i / 8;
+        byte col = i % 8;
+        gameboard[row][col] = (gameboard[row][col] + increment) % 8;
+        for (byte j = 0; j < row; j++)
+          gameboard[j][col] = (gameboard[j][col] + increment) % 8;
+        for (byte j = row + 1; j < 8; j++)
+          gameboard[j][col] = (gameboard[j][col] + increment) % 8;
+        for (byte j = 0; j < col; j++)
+          gameboard[row][j] = (gameboard[row][j] + increment) % 8;
+        for (byte j = col + 1; j < 8; j++)
+          gameboard[row][j] = (gameboard[row][j] + increment) % 8;
+        pressed[i] = (pressed[i] + increment) % 8;
+        showBoard(gameboard);
+        delay(300);
+      }
+    }
+  } while (!solved);
+}
+
 void setup()
 {
   delay(2000);
-  
+
   FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
   FastLED.setBrightness(50);
-  
+
+  pinMode(BUTTON_1, INPUT_PULLUP);
+  pinMode(BUTTON_2, INPUT_PULLUP);
+  pinMode(BUTTON_3, INPUT_PULLUP);
   pinMode(RESET_PIN, OUTPUT);
   pinMode(LD_PIN, OUTPUT);
   pinMode(SS_PIN, OUTPUT);
@@ -281,6 +317,7 @@ void setup()
   memset(gameboard, 0, sizeof gameboard);
   memset(debounce, 0, sizeof debounce);
   memset(button, 0, sizeof button);
+  increment = 1;
 
   attract();
   when = millis();
@@ -310,11 +347,24 @@ void readButtons()
     digitalWrite(SCLK_PIN, LOW);
     delayMicroseconds(10);
   }
+  if (!digitalRead(BUTTON_1)) {
+    increment = 1;
+  }
+  else if (!digitalRead(BUTTON_3)) {
+    increment = 7;
+  }
 }
 
 void loop()
 {
   readButtons();
+  if (!digitalRead(BUTTON_2)) {
+    solve();
+    delay(500);
+    attract();
+    increment = 1;
+    when = millis();
+  }
   unsigned long now = millis();
   if ((unsigned long)(now - when) > ATTRACT_TIME) {
     attract();
@@ -324,17 +374,18 @@ void loop()
       if (button[i]) {
         byte row = i / 8;
         byte col = i % 8;
-        gameboard[row][col] = (gameboard[row][col] + 1) % 8;
+        gameboard[row][col] = (gameboard[row][col] + increment) % 8;
         for (byte j = 0; j < row; j++)
-          gameboard[j][col] = (gameboard[j][col] + 1) % 8;
-        for (byte j = row+1; j < 8; j++)
-          gameboard[j][col] = (gameboard[j][col] + 1) % 8;
+          gameboard[j][col] = (gameboard[j][col] + increment) % 8;
+        for (byte j = row + 1; j < 8; j++)
+          gameboard[j][col] = (gameboard[j][col] + increment) % 8;
         for (byte j = 0; j < col; j++)
-          gameboard[row][j] = (gameboard[row][j] + 1) % 8;
-        for (byte j = col+1; j < 8; j++)
-          gameboard[row][j] = (gameboard[row][j] + 1) % 8;
-        when = now;
+          gameboard[row][j] = (gameboard[row][j] + increment) % 8;
+        for (byte j = col + 1; j < 8; j++)
+          gameboard[row][j] = (gameboard[row][j] + increment) % 8;
         button[i] = false;
+        pressed[i] = (pressed[i] + increment) % 8;
+        when = now;
       }
     }
   }
